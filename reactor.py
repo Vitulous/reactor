7f6c69b5dc1ce1c7cd200c3e7cd5afb701c1c567
Vitulous <44551327+Vitulous@users.noreply.github.com>
2018-11-28 11:48:22 +0200

    Update reactor.py

--- reactor.py
+++ reactor.py
@@ -3,15 +3,24 @@ import random
 import os
 import re
 import asyncio
-from apscheduler.schedulers.blocking import BlockingScheduler
+import requests as req
 
-sched = BlockingScheduler()
 client = discord.Client()
-
-@sched.scheduled_job('interval', minutes=3)
-async def timed_job():
-    await client.send_message(discord.Object(id='506143567400534016'), 'This job is run every three minutes.')
-
+async def daily():
+    count = 0
+    await client.wait_until_ready()
+    while not client.is_closed:
+        count += 1
+        msg = str(count)
+        await client.send_message(discord.Object(id='506143567400534016'), msg)
+        await asyncio.sleep(3600)
+'''async def alive():
+    await client.wait_until_ready()
+    while not client.is_closed:
+        url = 'http://randompersonal.forum.wtf/'
+        await asyncio.sleep(1800)
+        keep = req.get(url)'''
+    
 @client.event
 async def on_message(message):
      if message.author == client.user:
@@ -27,5 +36,5 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-sched.start()
+client.loop.create_task(daily())
 client.run(os.getenv('TOKEN'))
