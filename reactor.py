7bc3fdc5feb02afe34110bdd87668d7cd6576112
Vitulous <44551327+Vitulous@users.noreply.github.com>
2018-11-30 11:01:05 +0200

    Update reactor.py

--- reactor.py
+++ reactor.py
@@ -9,15 +9,15 @@ client = discord.Client()
 async def daily():
     await client.wait_until_ready()
     while not client.is_closed:
-        await asyncio.sleep(86400)
         await client.send_message(discord.Object(id='517088248053628929'), 'Новости дня: Кока - пидор.')
+        await asyncio.sleep(86400)
         
-'''async def alive():
+async def alive():
     await client.wait_until_ready()
     while not client.is_closed:
         url = 'http://randompersonal.forum.wtf/'
         await asyncio.sleep(1800)
-        keep = req.get(url)'''
+        keep = req.get(url)
     
 @client.event
 async def on_message(message):
@@ -56,4 +56,5 @@ async def on_ready():
     print(client.user.id)
     print('------')
 client.loop.create_task(daily())
+client.loop.create_task(alive())
 client.run(os.getenv('TOKEN'))
